404295e09b165f6a86b1887f10a051c7b84e9544
ashleighpresley <ashleighpresley1@gmail.com>
2022-04-18 23:36:49 -0500

    Styling FAQ section/ making accordion functional

---
+++ src/containers/faqs.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Accordion } from "../components";
+import faqsData from "../fixtures/faqs.json";
+
+export function FaqsContainer() {
+  return (
+    <Accordion>
+      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+      {faqsData.map((faq) => (
+        <Accordion.Item key={faq.id}>
+          <Accordion.Header>{faq.header}</Accordion.Header>
+          <Accordion.Body>{faq.body}</Accordion.Body>
+        </Accordion.Item>
+      ))}
+    </Accordion>
+  );
+}

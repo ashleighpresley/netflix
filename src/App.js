7576b46b185043db349b2f8e21a28c8047e1f98c
ashleighpresley <ashleighpresley1@gmail.com>
2022-04-19 11:19:58 -0500

    Adding logo and home, browse, sign in/up routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { JumbotronContainer } from "./containers/jumbotron";
-import { FooterContainer } from "./containers/footer";
-import { FaqsContainer } from "./containers/faqs";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home, Browse, SignIn, SignUp } from "./pages/index";
+import * as ROUTES from "./constants/routes";
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Routes>
+        <Route exact path={ROUTES.HOME} element={<Home />}></Route>
+        <Route exact path={ROUTES.BROWSE} element={<Browse />}></Route>
+        <Route exact path={ROUTES.SIGN_IN} element={<SignIn />}></Route>
+        <Route exact path={ROUTES.SIGN_UP} element={<SignUp />}></Route>
+      </Routes>
+    </Router>
   );
 }

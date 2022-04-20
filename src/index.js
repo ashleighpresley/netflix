9b1fc2a5e2e0c82414b81dcd615a83b2fe72c6cc
ashleighpresley <ashleighpresley1@gmail.com>
2022-04-20 00:31:53 -0500

    React Context for Firebase

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from "./App";
 import { GlobalStyles } from "./global-styles";
 import "normalize.css";
 import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <>
-      <GlobalStyles />
-      <App />
+      <FirebaseContext.Provider value={{ firebase }}>
+        <GlobalStyles />
+        <App />
+      </FirebaseContext.Provider>
     </>
   </React.StrictMode>
 );

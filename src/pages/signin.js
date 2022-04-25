ef3f24b5c22431271e042ba402f7c699067bb430
ashleighpresley <ashleighpresley1@gmail.com>
2022-04-25 12:15:07 -0500

    Set up sign in page + start work on Firebase auth

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,9 +1,69 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { FirebaseContext } from "../context/firebase";
+import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
 
 export default function SignIn() {
+  const { firebase } = useContext(FirebaseContext);
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid = password === "" || emailAddress === "";
+  const handleSignIn = (event) => {
+    event.preventDefault();
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        //push to the browse page
+      })
+      .catch((error) => {
+        setEmailAddress("");
+        setPassword("");
+        setError(error.message);
+      });
+  };
+
   return (
     <>
-      <p>Hello from the Sign In page</p>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignIn} method="POST"></Form.Base>
+          <Form.Input
+            required
+            placeholder="Email Address"
+            value={emailAddress}
+            onChange={({ target }) => setEmailAddress(target.value)}
+          />
+          <Form.Input
+            required
+            type="password"
+            autocomplete="off"
+            placeholder="Password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <Form.Submit disabled={isInvalid} type="submit">
+            Sign In
+          </Form.Submit>
+          <Form.Text>
+            New to Netflix? {""}
+            <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
     </>
   );
 }

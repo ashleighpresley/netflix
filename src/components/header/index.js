60862b285cc4233d80622866fec431c96a0c5a30
ashleighpresley <ashleighpresley1@gmail.com>
2022-04-26 12:46:48 -0500

    Styling browser header, add sign out button

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,45 @@
 import React from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
-import { Background, ButtonLink, Container, Logo } from "./styles/header";
+import {
+  Background,
+  ButtonLink,
+  Container,
+  Dropdown,
+  Feature,
+  FeatureCallOut,
+  Group,
+  Link,
+  Logo,
+  Text,
+  Picture,
+  Profile,
+} from "./styles/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
 }
 
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+  return <Feature {...restProps}>{children}</Feature>;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+  children,
+  ...restProps
+}) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
+};
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
 };
 
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
@@ -20,4 +52,25 @@ Header.Logo = function HeaderLogo({ to, ...restProps }) {
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
   return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return (
+    <Picture
+      {...restProps}
+      src={src ? `/images/users/${src}` : `/images/misc/loading.gif`}
+    />
+  );
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>;
 };
